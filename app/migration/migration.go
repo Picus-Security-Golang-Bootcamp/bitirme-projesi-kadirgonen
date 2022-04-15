1077f89b5a574a1c45f6ec1fb0e44310aa0558a4
Kadir GÖNEN <83732892+kadirgonen@users.noreply.github.com>
2022-04-16 01:01:36 +0300

    Upload to migration.go file
    Uploading migration.go file

---
+++ app/migration/migration.go
@@ -0,0 +1,71 @@
+package migration
+
+import (
+	"fmt"
+
+	model "HW/app/models"
+
+	"gorm.io/gorm"
+)
+
+var roles = []model.Role{
+	{Name: "admin"},
+	{Name: "customer"},
+}
+
+var users = []model.User{
+	{Email: "kg@kgstore.com", Password: "kg1453", Roles: []*model.Role{&roles[0]}},
+}
+
+var categories = []model.Category{
+	{Name: "Bilgisayar", ID: 1},
+}
+
+func Execute(db *gorm.DB) error {
+	// Check if migration done
+	tables, err := db.Migrator().GetTables()
+	if err != nil {
+		return fmt.Errorf("seeder - Load - db.Migrator.GetTables: %w", err)
+	}
+
+	if len(tables) == 0 {
+		// Auto create tables
+		err = db.AutoMigrate(
+			&model.User{},
+			&model.Role{},
+			&model.Cart{},
+			&model.CartItem{},
+			&model.Category{},
+			&model.Product{},
+			&model.Order{},
+			&model.OrderItem{},
+		)
+
+		if err != nil {
+			return fmt.Errorf("seeder - Load - db.AutoMigrate: %w", err)
+		}
+
+		for i := range categories {
+			err := db.Model(&model.Category{}).Create(&categories[i]).Error
+			if err != nil {
+				return fmt.Errorf("seeder - Load - Model(Category).Create: %w", err)
+			}
+		}
+
+		for i := range roles {
+			err := db.Model(&model.Role{}).Create(&roles[i]).Error
+			if err != nil {
+				return fmt.Errorf("seeder - Load - Model(Role).Create: %w", err)
+			}
+		}
+
+		for i := range users {
+			err := db.Model(&model.User{}).Create(&users[i]).Error
+			if err != nil {
+				return fmt.Errorf("seeder - Load - Model(User).Create: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
