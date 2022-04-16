a154f17a2b3d2eddd268db3cf328783cc9eb32f3
Kadir GÖNEN <83732892+kadirgonen@users.noreply.github.com>
2022-04-17 02:24:59 +0300

    Upload to order.go file
    Uploading order.go file for handler

---
+++ app/handler/order.go
@@ -0,0 +1,89 @@
+package handler
+
+import (
+	"net/http"
+
+	model "HW/app/models"
+	"HW/app/service"
+	"HW/pkg/logger"
+
+	"github.com/gin-gonic/gin"
+)
+
+type (
+	Order struct {
+		orderService service.OrderService
+		logger       logger.Logger
+	}
+
+	newOrderRequest struct {
+		Name        string `json:"name" binding:"required"`
+		Address     string `json:"address" binding:"required"`
+		PhoneNumber string `json:"phone_number" binding:"required"`
+	}
+
+	orderResponse struct {
+		ID    string             `json:"id"`
+		Items []*model.OrderItem `json:"items"`
+	}
+)
+
+func NewOrder(os service.OrderService, l logger.Logger) *Order {
+	return &Order{os, l}
+}
+
+func (c *Order) GetAllOrders(g *gin.Context) {
+	userName := g.GetString("Email")
+	if len(userName) == 0 {
+		errorResponse(g, http.StatusBadRequest, "unable to get parameters")
+		return
+	}
+
+	orders := c.orderService.GetAllOrders(userName)
+	g.JSON(http.StatusOK, orders)
+}
+
+func (c *Order) CreateOrder(g *gin.Context) {
+	userName := g.GetString("Email")
+	if len(userName) == 0 {
+		errorResponse(g, http.StatusBadRequest, "unable to get parameters")
+		return
+	}
+
+	var req newOrderRequest
+	if err := g.ShouldBind(&req); err != nil {
+		c.logger.Error(err, "http - createOrder")
+		errorResponse(g, http.StatusBadRequest, "invalid request body")
+		return
+	}
+
+	err := c.orderService.CreateOrder(userName, req.Name, req.Address, req.PhoneNumber)
+	if err != nil {
+		errorResponse(g, http.StatusNotFound, err.Error())
+		return
+	}
+
+	successResponse(g, http.StatusOK, "Operation completed successfully.")
+}
+
+func (c *Order) CancelOrder(g *gin.Context) {
+	userName := g.GetString("Email")
+	if len(userName) == 0 {
+		errorResponse(g, http.StatusBadRequest, "unable to get parameters")
+		return
+	}
+
+	id := g.Param("id")
+	if len(id) == 0 {
+		errorResponse(g, http.StatusBadRequest, "unable to get parameters")
+		return
+	}
+
+	err := c.orderService.CancelOrder(userName, id)
+	if err != nil {
+		errorResponse(g, http.StatusNotFound, err.Error())
+		return
+	}
+
+	successResponse(g, http.StatusOK, "Operation completed successfully.")
+}
