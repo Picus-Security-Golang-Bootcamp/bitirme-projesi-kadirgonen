28ce018b5c8de06903e9c9095811302ced130668
Kadir GÖNEN <83732892+kadirgonen@users.noreply.github.com>
2022-04-17 00:48:17 +0300

    Upload to product.go file
    Uploading product.go file for service

---
+++ app/service/product.go
@@ -0,0 +1,67 @@
+package service
+
+import (
+	"errors"
+
+	model "HW/app/models"
+	"HW/app/repo"
+)
+
+type ProductService struct {
+	productRepo repo.ProductRepository
+}
+
+func NewStoreService(pr repo.ProductRepository) *ProductService {
+	return &ProductService{
+		productRepo: pr,
+	}
+}
+func (s *ProductService) GetAllProducts(pageIndex, pageSize int) ([]model.Product, int) {
+	items, count := s.productRepo.GetAllProducts(pageIndex, pageSize)
+
+	return items, count
+}
+
+func (s *ProductService) GetProduct(productId int) *model.Product {
+	return s.productRepo.GetProduct(productId)
+}
+
+func (s *ProductService) GetProductStockCode(StockCode string) *model.Product {
+	return s.productRepo.GetProductStockCode(StockCode)
+}
+
+func (s *ProductService) SearchProducts(query string) []model.Product {
+	return s.productRepo.SearchProduct(query)
+}
+
+func (s *ProductService) CreateProduct(product *model.Product) error {
+	productExists := s.productRepo.GetProductStockCode(product.StockCode)
+	if productExists != nil {
+		return errors.New("product with same sku already exist in database")
+	}
+
+	err := s.productRepo.CreateProduct(product)
+	if err != nil {
+		return errors.New("an unknown error occurred during operation")
+	}
+
+	return nil
+}
+
+func (s *ProductService) UpdateProduct(product *model.Product) error {
+	err := s.productRepo.UpdateProduct(product)
+	if err != nil {
+		return errors.New("an unknown error occurred during operation")
+	}
+
+	return nil
+}
+
+func (s *ProductService) DeleteProduct(productId int) error {
+	err := s.productRepo.DeleteProduct(productId)
+	if err != nil {
+		return errors.New("an unknown error occurred during operation")
+	}
+
+	return nil
+}
