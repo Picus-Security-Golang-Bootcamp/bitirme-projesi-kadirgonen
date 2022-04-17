edf2d5ba1fa9c898396871b196f7c3d05fabd9f7
Kadir GÖNEN <83732892+kadirgonen@users.noreply.github.com>
2022-04-17 18:15:21 +0300

    Change to config.go file knowledges
    Change to config.go file knowledges

--- config/config.go
+++ config/config.go
@@ -1,21 +1,91 @@
 package config
 
 import (
-	"fmt"
+	"errors"
+	"log"
+	"time"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/spf13/viper"
 )
 
-var DB *gorm.DB
-var err error
+type Config struct {
+	ServerConfig ServerConfig
+	JWTConfig    JWTConfig
+	DBConfig     DBConfig
+	Logger       Logger
+	Migration    Migration
+	Gin          Gin
+	Http         Http
+}
 
-const db_string = "host=localhost user=postgres password=2236386alper dbname=PatikaGoDB port=5432 sslmode=disable"
+type ServerConfig struct {
+	AppVersion       string
+	Mode             string
+	RoutePrefix      string
+	Debug            bool
+	Port             string
+	TimeoutSecs      int64
+	ReadTimeoutSecs  int64
+	WriteTimeoutSecs int64
+}
 
-func DbConfiguration() {
-	DB, err = gorm.Open(postgres.Open(db_string), &gorm.Config{})
-	if err != nil {
-		fmt.Println(err.Error())
+type JWTConfig struct {
+	JWTSecret string
+	JWTIss    string
+	JWTExp    time.Duration
+}
+
+type DBConfig struct {
+	DataSourceName string
+	DBHost         string
+	DBPort         string
+	DBDatabaseName string
+	DBUsername     string
+	DBPassword     string
+	MaxOpen        int
+	MaxIdle        int
+	MaxLifetime    int
+}
+
+// Logger config
+type Logger struct {
+	Development bool
+	Encoding    string
+	Level       string
+}
+
+type Migration struct {
+	AutoMigrate bool
+}
+type Gin struct {
+	GINMode string
+}
+type Http struct {
+	HTTPPort string
+}
+
+// LoadConfig file from given path
+func LoadConfig(filename string) (*Config, error) {
+	v := viper.New()
+
+	v.SetConfigName(filename)
+	v.SetConfigType("yaml")
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
 	}
+
+	var c Config
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Printf("unable to decode into struct, %v", err)
+		return nil, err
+	}
+
+	return &c, nil
 
 }
