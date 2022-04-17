b46e9c469500113b03f521cdd23f77e8869b9bd4
Kadir GÖNEN <83732892+kadirgonen@users.noreply.github.com>
2022-04-17 21:16:50 +0300

    Correct to validate token errors
    Correct to validate token errors

--- app/middleware/user_middleware.go
+++ app/middleware/user_middleware.go
@@ -27,16 +27,13 @@ func NewJWTAuthMiddleware(as service.JWTAuthService, us service.UserService, log
 func (m *JWTAuthMiddleware) ValidateToken() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.Request.Header.Get("Authorization")
-		tokenParts := strings.Split(authHeader, " ")
-		if len(tokenParts) == 2 {
-			authorized, claims := m.authService.VerifyToken(tokenParts[1])
+			authorized, claims := m.authService.VerifyToken(authHeader)
 			if authorized {
 				c.Set("UserId", claims.UserId)
 				c.Set("Email", claims.Email)
 				c.Next()
 				return
 			}
-		}
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"error": "unauthorized access",
 		})
