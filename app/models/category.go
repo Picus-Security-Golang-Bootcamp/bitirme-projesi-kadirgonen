1646d72ffaf81c605471797459ca7e9f313a7af3
Kadir GÖNEN <83732892+kadirgonen@users.noreply.github.com>
2022-04-17 20:18:43 +0300

    Correcting struct errors
    Correcting struct errors

--- app/models/category.go
+++ app/models/category.go
@@ -11,9 +11,6 @@ type Category struct {
 	ID         int       `gorm:"primary_key;" json:"id"`
 	Name       string    `json:"name"`
 	Products   []Product `gorm:"foreignkey:CategoryID" json:"products"`
-	Created_at time.Time `json:"created_at"`
-	Updated_at time.Time `json:"updated_at"`
-	Deleted_at time.Time `json:"deleted_at"`
 }
 
 func NewCategory(id int, name string) *Category {
