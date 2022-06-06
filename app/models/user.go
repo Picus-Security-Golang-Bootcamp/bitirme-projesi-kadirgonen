1ebccb7237517c300a90c3f989f4dfb2816e8dbe
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-06-06 23:59:45 +0300

    Correct to errors
    Correct to errors

--- app/models/user.go
+++ app/models/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	gorm.Model
-	ID         uint      `gorm:"primary_key;" json:"id"`
+	ID         int      `gorm:"primary_key;" json:"id"`
 	Name       string    `json:"name"`
 	Email      string    `json:"email"`
 	Password   string    `json:"-"`
